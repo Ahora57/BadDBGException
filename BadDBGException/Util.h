7d8e0fd91ea26307f00d1ef7c4902972b7165c1d
Ahora57 <84189666+Ahora57@users.noreply.github.com>
2022-02-15 00:35:00 +0300

    Support work under windows 8.1

--- BadDBGException/Util.h
+++ BadDBGException/Util.h
@@ -24,27 +24,18 @@ namespace Util
 				auto current_address = static_cast<intptr_t>(align_page) + index;
 
 				/*
-				KiShadowProcessorAllocation
-				48 8D ? ? ? ? FF 48 8B ? E8 ? ? ?  FF 4C 8D 05  ? ? ? ?  48 8B ?  44 ? ? 48 8B C8 E8
-				work 7 sp1,10,11 windows pattern unique and should  be work ? (7,8.1 and maybe 8 don't have export KiShadowProcessorAllocation)
+				Windows 7 - 11 support
+				VfPowerDumpIrpStack sig:
+				48 8D 3D ? ? ? FF 48 8B 8C ? ? ? ? ? E8
 				*/
 				if (*reinterpret_cast<uint8_t*>(current_address) == 0x48
 					&& *reinterpret_cast<uint8_t*>(current_address + 1) == 0x8D
+					&& *reinterpret_cast<uint8_t*>(current_address + 2) == 0x3D
 					&& *reinterpret_cast<uint8_t*>(current_address + 6) == 0xFF
 					&& *reinterpret_cast<uint8_t*>(current_address + 7) == 0x48
 					&& *reinterpret_cast<uint8_t*>(current_address + 8) == 0x8B
-					&& *reinterpret_cast<uint8_t*>(current_address + 10) == 0xE8
-					&& *reinterpret_cast<uint8_t*>(current_address + 14) == 0xFF
-					&& *reinterpret_cast<uint8_t*>(current_address + 15) == 0x4C
-					&& *reinterpret_cast<uint8_t*>(current_address + 16) == 0x8D
-					&& *reinterpret_cast<uint8_t*>(current_address + 17) == 0x5
-					&& *reinterpret_cast<uint8_t*>(current_address + 22) == 0x48
-					&& *reinterpret_cast<uint8_t*>(current_address + 23) == 0x8B
-					&& *reinterpret_cast<uint8_t*>(current_address + 25) == 0x44
-					&& *reinterpret_cast<uint8_t*>(current_address + 28) == 0x48
-					&& *reinterpret_cast<uint8_t*>(current_address + 29) == 0x8B
-					&& *reinterpret_cast<uint8_t*>(current_address + 30) == 0xC8
-					&& *reinterpret_cast<uint8_t*>(current_address + 31) == 0xE8
+					&& *reinterpret_cast<uint8_t*>(current_address + 9) == 0x8C
+					&& *reinterpret_cast<uint8_t*>(current_address + 15) == 0xE8
 
 					)
 				{
